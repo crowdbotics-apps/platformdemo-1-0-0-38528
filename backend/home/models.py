fd44c8e1bb14d6d56226034a08c55e4f0a967f59
crowdbotics-apps <team@crowdbotics.com>
2023-02-03 21:40:41 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,6 @@
+from django.conf import settings
 from django.db import models
+class Profile(models.Model):
+    'Generated Model'
+    phone_number = models.CharField(max_length=25,)
+    ss_id = models.BigIntegerField()
