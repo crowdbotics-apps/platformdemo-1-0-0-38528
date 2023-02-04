115dd9ce1faa584c0e7df6419627421afd2e5e9a
crowdbotics-apps <team@crowdbotics.com>
2023-02-04 00:07:23 +0000

    Update models

--- backend/home/models.py
+++ backend/home/models.py
@@ -4,3 +4,4 @@ class Profile(models.Model):
     'Generated Model'
     phone_number = models.CharField(max_length=25,)
     ss_id = models.BigIntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,null=True,blank=True,related_name="profile_user",)
