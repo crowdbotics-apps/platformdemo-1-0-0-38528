c41cd060aa9cc9327c892b452187633cf84d6492
shahraiz-cb <shahraiz.ali@crowdbotics.com>
2023-02-22 18:21:35 +0100

    Crowdbotics Studio: Update app screens

--- screens/Untitled2/index.js
+++ screens/Untitled2/index.js
@@ -9,7 +9,7 @@ import { StyleSheet, SafeAreaView } from "react-native";
 const Untitled2 = () => {
   return <SafeAreaView style={styles.safeArea}>
         
-    <View style={styles.EENXZKxE}><ImageBackground style={styles.INXyNypY} source={require("./photo-1596387451750-f7bfb51461ef.jpg")} resizeMode="cover"></ImageBackground></View><View style={styles.iCSCmWJT}><View style={styles.hzgHOJit}><ImageBackground style={styles.llYtiqRC} source={require("./profile-1495388545592-e4e376925c59.jpg")} resizeMode="cover"></ImageBackground></View></View><Text style={styles.ktUqwGtB}>Name</Text><TextInput style={styles.AvjfIyrm} value="" placeholder="Enter Name" placeholderTextColor="#aeacac"></TextInput><TextInput style={styles.bIOESxrb}></TextInput><CheckBox style={styles.vkaDzBNS} title="Accept Terms" size={20} checked=""></CheckBox></SafeAreaView>;
+    <View style={styles.EENXZKxE}><ImageBackground style={styles.INXyNypY} source={require("./photo-1596387451750-f7bfb51461ef.jpg")} resizeMode="cover"></ImageBackground></View><View style={styles.iCSCmWJT}><View style={styles.hzgHOJit}><ImageBackground style={styles.llYtiqRC} source={require("./profile-1495388545592-e4e376925c59.jpg")} resizeMode="cover"></ImageBackground></View></View><Text style={styles.ktUqwGtB}>Name</Text><TextInput style={styles.AvjfIyrm} value="" placeholder="Enter Name" placeholderTextColor="#aeacac"></TextInput><CheckBox style={styles.vkaDzBNS} title="Accept Terms" size={20} checked=""></CheckBox></SafeAreaView>;
 };
 
 const styles = StyleSheet.create({
